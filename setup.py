8347ee78e56a7b6b2302fe6c6ecc13c7892edd45
Devin Dooley <devinadooley@gmail.com>
2018-07-22 14:03:50 -0700

    rename and authentication added

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(
-    name='websiteimproved',
-    packages=['websiteimproved'],
+    name='dumbnews',
+    packages=['dumbnews'],
     include_package_data=True,
     install_requires=[
         'flask',
--- websiteimproved/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def landing():
-    return 'Welcome to my website'
